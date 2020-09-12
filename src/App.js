c0bb03063edc8495a146e55db7fa3ebee4815aea
Besim Gurbuz <mbesimgurbuz@gmail.com>
2020-09-12 03:47:57 +0300

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
 
 function App() {
-  // BEM naming convention
   return (
     <div className="app">
-      <div className="app__body">
-        {/* Sidebar */}
-        {/* Chat */}
+      <div className="app_body">
+        <Router>
+          <Sidebar/>
+          <Switch>
+            <Route path="/rooms/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h2>Welcome to the app</h2>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </div>
   );
